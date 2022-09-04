a0af64a84ddc9ce478661988c447a7a4e77ddfc5
msw <mswl@ya.ru>
2022-09-04 15:43:08 +0300

    fix quantity

--- ufmt/quantity.go
+++ ufmt/quantity.go
@@ -4,52 +4,76 @@ import "github.com/msw-x/moon/umath"
 
 func ByteSize[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
+		Base: 1024,
 		Name: "B",
+	})
+}
+
+func ByteSizeExact[V umath.AnyInt](v V) string {
+	return Int(v, IntCtx{
+		Base:     1024,
+		Name:     "B",
+		MaxLevel: 0,
 	})
 }
 
 func ByteSizeDense[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
-		Dense: true,
+		Base:  1024,
 		Name:  "B",
+		Dense: true,
 	})
 }
 
 func ByteSpeed[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
+		Base: 1024,
 		Name: "B/s",
 	})
 }
 
 func ByteSpeedDense[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
-		Dense: true,
+		Base:  1024,
 		Name:  "B/s",
+		Dense: true,
 	})
 }
 
 func BitSize[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
+		Base: 1024,
 		Name: "b",
+	})
+}
+
+func BitSizeExact[V umath.AnyInt](v V) string {
+	return Int(v, IntCtx{
+		Base:     1024,
+		Name:     "b",
+		MaxLevel: 0,
 	})
 }
 
 func BitSizeDense[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
-		Dense: true,
+		Base:  1024,
 		Name:  "b",
+		Dense: true,
 	})
 }
 
 func BitSpeed[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
+		Base: 1024,
 		Name: "b/s",
 	})
 }
 
 func BitSpeedDense[V umath.AnyInt](v V) string {
 	return Int(v, IntCtx{
-		Dense: true,
+		Base:  1024,
 		Name:  "b/s",
+		Dense: true,
 	})
 }
