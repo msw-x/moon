1645f473d0e1797cb6bef48794061d9d369526b6
msw <mswl@ya.ru>
2022-10-04 09:19:45 +0300

    app RunJust

--- app/run.go
+++ app/run.go
@@ -7,27 +7,23 @@ import (
 	"github.com/msw-x/moon/ulog"
 )
 
-func Run[UserConf any](version string, run func(UserConf)) {
+func Run[UserConf any](version string, fn func(UserConf)) {
 	defer ulog.Close()
 	defer moon.Recover(Fatal)
 	if confFile, ok := ParseCmdLine(version); ok {
 		conf := LoadConf[Conf](confFile)
-		ulog.Init(conf.Log())
-		log := ulog.New("app")
-		defer log.Close()
-		log.Info("startup")
-		log.Info("version:", version)
-		log.Info("pid:", Pid())
-		log.Info("os:", OS())
-		log.Info("arch:", Arch())
-		log.Info("name:", Name())
-		log.Info("path:", Dir())
-		log.Info("conf:", confFile)
-		userConf := LoadConf[UserConf](confFile)
-		run(userConf)
-		log.Info(ulog.Stat())
-		log.Info("shutdown")
+		run(version, conf.Log(), func(log *ulog.Log) {
+			log.Info("conf:", confFile)
+			userConf := LoadConf[UserConf](confFile)
+			fn(userConf)
+		})
 	}
+}
+
+func RunJust(version string, conf ulog.Conf, fn func()) {
+	defer ulog.Close()
+	defer moon.Recover(Fatal)
+	run(version, conf, fn)
 }
 
 func Go(fn func()) {
@@ -40,4 +36,27 @@ func Go(fn func()) {
 func Fatal(s string) {
 	ulog.Critical(s)
 	os.Exit(1)
+}
+
+func run(version string, conf ulog.Conf, fn any) {
+	ulog.Init(conf)
+	log := ulog.New("app")
+	defer log.Close()
+	log.Info("startup")
+	log.Info("version:", version)
+	log.Info("pid:", Pid())
+	log.Info("os:", OS())
+	log.Info("arch:", Arch())
+	log.Info("name:", Name())
+	log.Info("path:", Dir())
+	switch f := fn.(type) {
+	case func():
+		f()
+	case func(*ulog.Log):
+		f(log)
+	default:
+		panic("app run: invalid callback")
+	}
+	log.Info(ulog.Stat())
+	log.Info("shutdown")
 }
