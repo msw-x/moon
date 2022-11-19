025d5cc35317810f2a3da431c722896bdc66f69e
msw <mswl@ya.ru>
2022-11-19 18:31:53 +0300

    any float

--- umath/any.go
+++ umath/any.go
@@ -4,6 +4,10 @@ type AnyInt interface {
 	int | int32 | uint32 | int64 | uint64
 }
 
+type AnyFloat interface {
+	float32 | float64
+}
+
 type AnyNumber interface {
-	AnyInt | float32 | float64
+	AnyInt | AnyFloat
 }
