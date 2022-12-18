f8f0c5602a6b76de9022e32214b40c8c38992926
msw <mswl@ya.ru>
2022-12-18 18:08:29 +0300

    go do

---
+++ app/go.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"time"
+
+	"github.com/msw-x/moon"
+	"github.com/msw-x/moon/ulog"
+	"github.com/msw-x/moon/usync"
+)
+
+func Go(fn func()) GoDo {
+	do := usync.NewDo()
+	go func() {
+		defer do.Notify()
+		defer moon.Recover(func(e string) {
+			ulog.Critical(e)
+		})
+		fn()
+	}()
+	return do
+}
+
+type GoDo interface {
+	Do() bool
+	Stop()
+	Cancel()
+	Sleep(timeout time.Duration)
+}
