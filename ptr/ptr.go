db709f6a49fef7ba3e2ed5bd20c28dea0629070c
msw <mswl@ya.ru>
2023-02-05 17:20:12 +0300

    fix ptr

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -4,21 +4,21 @@ func From[T any](v T) *T {
 	return &v
 }
 
-func To[T any](p *T) T {
+func Value[T any](p *T) T {
 	var v T
-	return ToValueDef(p, v)
+	return ValueDef(p, v)
 }
 
-func ToDef[T any](p *T, v T) T {
+func ValueDef[T any](p *T, v T) T {
 	if p != nil {
 		v = *p
 	}
 	return v
 }
 
-func EqualValues[T any](a, b *T) bool {
+func Equal[T any](a, b *T) bool {
 	if a == b {
 		return true
 	}
-	return ToValue(a) == ToValue(b)
+	return Value(a) == Value(b)
 }
