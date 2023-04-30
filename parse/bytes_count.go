ce15c8b133b218f68a7ae191c262ff84333fdaf0
msw <mswl@ya.ru>
2023-04-30 18:19:38 +0300

    bytes count

---
+++ parse/bytes_count.go
@@ -0,0 +1,38 @@
+package parse
+
+import (
+	"math"
+
+	"github.com/msw-x/moon"
+)
+
+func BytesCount(s string) (v uint64, err error) {
+	var multiplier uint64
+	if len(s) > 0 {
+		var level int
+		suf := s[len(s)-1:]
+		switch suf {
+		case "K":
+			level = 1
+		case "M":
+			level = 2
+		case "G":
+			level = 3
+		case "T":
+			level = 4
+		}
+		if level > 0 {
+			s = s[:len(s)-1]
+		}
+		multiplier = uint64(math.Pow(1024, float64(level)))
+	}
+	v, err = Uint64(s)
+	v *= multiplier
+	return
+}
+
+func BytesCountStrict(s string) uint64 {
+	n, err := BytesCount(s)
+	moon.Strict(err, "parse bytes count")
+	return n
+}
