24a77472e75023987f1b625848370f0a528e5a7a
msw <mswl@ya.ru>
2023-05-10 15:24:47 +0300

    update diff

--- diff/slice.go
+++ diff/slice.go
@@ -1,37 +1,37 @@
 package diff
 
 type Slice[T any] struct {
-    OnCompare func(a, b T) bool
-    OnDelete  func(T)
-    OnInsert  func(T)
-    OnUpdate  func(prev, curr T)
+	OnCompare func(a, b T) bool
+	OnDelete  func(T)
+	OnInsert  func(T)
+	OnUpdate  func(prev, curr T)
 }
 
 func (o Slice[T]) Compare(prev []T, curr []T) {
-    for _, s := range prev {
-        if !o.find(curr, s, true) {
-            if o.OnDelete != nil {
-                o.OnDelete(s)
-            }
-        }
-    }
-    for _, m := range curr {
-        if !o.find(prev, m, false) {
-            if o.OnInsert != nil {
-                o.OnInsert(m)
-            }
-        }
-    }
+	for _, s := range prev {
+		if !o.find(curr, s, true) {
+			if o.OnDelete != nil {
+				o.OnDelete(s)
+			}
+		}
+	}
+	for _, m := range curr {
+		if !o.find(prev, m, false) {
+			if o.OnInsert != nil {
+				o.OnInsert(m)
+			}
+		}
+	}
 }
 
 func (o Slice[T]) find(s []T, what T, byCurr bool) bool {
-    for _, v := range s {
-        if o.OnCompare(v, what) {
-            if byCurr && o.OnUpdate != nil {
-                o.OnUpdate(what, v)
-            }
-            return true
-        }
-    }
-    return false
+	for _, v := range s {
+		if o.OnCompare(v, what) {
+			if byCurr && o.OnUpdate != nil {
+				o.OnUpdate(what, v)
+			}
+			return true
+		}
+	}
+	return false
 }
