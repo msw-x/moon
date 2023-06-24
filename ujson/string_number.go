f8f7fc911b44ef699e628a501e8cb5e66e658eeb
msw <mswl@ya.ru>
2023-06-24 18:55:46 +0300

    ujson default value of string number

--- ujson/string_number.go
+++ ujson/string_number.go
@@ -34,6 +34,19 @@ func (o StringInt64) Value() (int64, error) {
 	return parse.Int64(string(o))
 }
 
+func (o StringInt64) ValueOr(v int64) int64 {
+	x, err := o.Value()
+	if err == nil {
+		return x
+	}
+	return v
+}
+
+func (o StringInt64) ValueOrDefault() int64 {
+	var v int64
+	return o.ValueOr(v)
+}
+
 type StringFloat64 string
 
 func (o StringFloat64) Empty() bool {
@@ -46,4 +59,17 @@ func (o StringFloat64) Exists() bool {
 
 func (o StringFloat64) Value() (float64, error) {
 	return parse.Float64(string(o))
+}
+
+func (o StringFloat64) ValueOr(v float64) float64 {
+	x, err := o.Value()
+	if err == nil {
+		return x
+	}
+	return v
+}
+
+func (o StringFloat64) ValueOrDefault() float64 {
+	var v float64
+	return o.ValueOr(v)
 }
