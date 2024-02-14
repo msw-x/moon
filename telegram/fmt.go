76dfe92c9f517dce66c3dcc8414d814ae0eb968c
msw <mswl@ya.ru>
2024-02-14 11:16:56 +0300

    telegram fmt

--- telegram/fmt.go
+++ telegram/fmt.go
@@ -1,6 +1,10 @@
 package telegram
 
-import "fmt"
+import (
+	"fmt"
+
+	botapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func Bold(v any) string {
 	return fmt.Sprintf("***%v***", v)
@@ -11,9 +15,21 @@ func Italic(v any) string {
 }
 
 func Monospace(v any) string {
-	return fmt.Sprintf("```%```", v)
+	return fmt.Sprintf("```%v```", v)
+}
+
+func Strikethrough(v any) string {
+	return fmt.Sprintf("~~~%v~~~", v)
+}
+
+func Spoiler(v any) string {
+	return fmt.Sprintf("||%v||", v)
 }
 
 func Ref(v any, url string) string {
 	return fmt.Sprintf("[%v](%s)", v, url)
+}
+
+func EscapeMarkdown(text string) string {
+	return botapi.EscapeText(botapi.ModeMarkdownV2, text)
 }
