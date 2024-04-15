63921564f7d93d2aa6d263715ff6284805eb92a5
msw <mswl@ya.ru>
2024-04-15 14:53:49 +0300

    ptr NilIfDef

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -16,6 +16,14 @@ func ToDef[T any](p *T, v T) T {
 	return v
 }
 
+func NilIfDef[T comparable](v T) *T {
+	var def T
+	if v == def {
+		return nil
+	}
+	return Of(v)
+}
+
 func Equal[T comparable](a, b *T) bool {
 	if a != nil && b != nil {
 		return *a == *b
