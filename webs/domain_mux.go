a23c74ba601588f9ff6746ffaacc557087ca5441
msw <mswl@ya.ru>
2024-06-07 11:25:39 +0300

    log reverse proxy

--- webs/domain_mux.go
+++ webs/domain_mux.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+
+	"github.com/msw-x/moon/ulog"
 )
 
 type DomainMux struct {
@@ -25,11 +27,28 @@ func (o *DomainMux) ReverseProxy(domain string, target string) error {
 }
 
 func (o *DomainMux) PureReverseProxy(domain string, target string) error {
+	log := ulog.New("reverse-proxy").WithID(domain)
 	targetUrl, err := url.Parse(target)
 	if err == nil {
 		o.m[domain] = &httputil.ReverseProxy{
 			Rewrite: func(r *httputil.ProxyRequest) {
 				r.SetURL(targetUrl)
+			},
+			ModifyResponse: func(v *http.Response) error {
+				r := v.Request
+				if r == nil {
+					log.Error("request is nil")
+				} else {
+					if v.StatusCode == http.StatusOK {
+						log.Debug(r.Method, r.URL, v.StatusCode)
+					} else {
+						log.Error(r.Method, r.URL, v.StatusCode)
+					}
+				}
+				return nil
+			},
+			ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
+				log.Errorf("%s %v: %v", r.Method, r.URL, err)
 			},
 		}
 	}
