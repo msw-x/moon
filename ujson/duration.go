41bfe56a41b421743c831703d0e1aa58834600b3
msw <mswl@ya.ru>
2024-10-23 10:56:21 +0300

    marshal json duration

--- ujson/duration.go
+++ ujson/duration.go
@@ -12,6 +12,14 @@ func (o Duration) Std() time.Duration {
 	return time.Duration(o)
 }
 
+func (o Duration) String() string {
+	return time.Duration(o).String()
+}
+
+func (o Duration) MarshalJSON() ([]byte, error) {
+	return quote(o.String()), nil
+}
+
 func (o *Duration) UnmarshalJSON(b []byte) error {
 	s := string(b)
 	s = ustring.TrimQuotes(s)
