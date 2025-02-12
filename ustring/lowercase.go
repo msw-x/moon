91f2245fbe0ff71938acfa9b808d6cd8d252e413
msw <mswl@ya.ru>
2025-02-12 16:41:59 +0300

    is uppper/lower

--- ustring/lowercase.go
+++ ustring/lowercase.go
@@ -1,10 +1,31 @@
 package ustring
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func TitleLowerCase(s string) string {
 	if len(s) > 0 {
 		s = strings.ToLower(string(s[0])) + s[1:]
 	}
 	return s
+}
+
+func IsUpper(s string) bool {
+	for _, r := range s {
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsLower(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
